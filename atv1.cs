6f1f95a1f1182e0778ba9b542a24efb9816a1ce7
vigs123 <giovanna.dantas@escolar.ifrn.edu.br>
2024-08-13 15:38:28 -0300

    Questão 4, 5 e 6
    4 - Faça um programa que leia um número do usuário e informe se ele é positivo, negativo ou zero.
    
    5 - Crie um programa que exiba os números de 1 a 10 usando um loop for.
    
    6 - Escreva um programa que leia a idade de uma pessoa e verifique se ela é maior de idade (18 anos ou mais) e se está apta a votar e a dirigir.

--- atv1.cs
+++ atv1.cs
@@ -34,3 +34,46 @@ string n = (Console.ReadLine());
 int i = int.Parse(Console.ReadLine());
 
 Console.WriteLine ($"Olá, {n}, você tem {i} anos.");
+
+//questão 4
+
+Console.WriteLine("Digite um número: ");
+        double x = double.Parse(Console.ReadLine());
+
+        if (x > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else if (x < 0)
+        {
+            Console.WriteLine("O número é negativo.");
+        }
+        else
+        {
+            Console.WriteLine("O número é zero.");
+        }
+
+// questão 5
+
+for (int I = 1; I <= 10; I++)
+    {
+        Console.WriteLine(I);
+    }
+
+// questão 6
+
+ Console.WriteLine("Digite a sua idade: ");
+    int idade = int.Parse(Console.ReadLine());
+
+     if (idade >= 18)
+    {
+        Console.WriteLine("Você é maior de idade, você está apto(a) a votar e a dirigir.");
+    }
+        else if (idade >= 16 && idade < 18)
+    {
+         Console.WriteLine("Você é menor de idade, mas já pode votar, poeém não esta apto(a) a ditigir");
+    }
+        else
+    {
+        Console.WriteLine("Você é menor de idade, você ainda não está apto(a) a votar ou a dirigir");
+    }
