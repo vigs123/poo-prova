077127ac37dadd88ea66b9adacd3b1465d2c71db
vigs123 <giovanna.dantas@escolar.ifrn.edu.br>
2024-08-13 15:10:27 -0300

    Questão 2
    Crie um programa que leia dois números inteiros do usuário e exiba a soma, subtração, multiplicação e divisão desses números.

--- atv1.cs
+++ atv1.cs
@@ -1,3 +1,5 @@
+//questão 1
+
 int a = 15;
 float b = 2.34f;
 string c = "HELLO!";
@@ -7,3 +9,19 @@ bool check = true;
  Console.WriteLine(b);
  Console.WriteLine(c);
  Console.WriteLine(check);
+
+//quetão 2
+
+Console.WriteLine ("Digite dois numeros inteiros");
+
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int S = A + B;
+int D = A / B;
+int M = A * B;
+int Sub = A-B;
+
+Console.WriteLine("Soma = " + S);
+Console.WriteLine("Subtração = " + Sub);
+Console.WriteLine("Divisão = " + D);
+Console.WriteLine("Multiplicação = " + M);
