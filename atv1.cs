62f6923fb80a66c2aa0d88c269d7c0a552cbd3f9
vigs123 <giovanna.dantas@escolar.ifrn.edu.br>
2024-08-13 15:52:55 -0300

    questão 7
    Implemente uma função em C# chamada CalcularMedia que recebe três notas (float) como parâmetros e retorna a média dessas notas. Chame essa função a partir do método Main e exiba o resultado no console.

--- atv1.cs
+++ atv1.cs
@@ -1,9 +1,9 @@
 //questão 1
 
-int a = 15;
-float b = 2.34f;
-string c = "HELLO!";
-bool check = true;
+ int a = 15;
+ float b = 2.34f;
+ string c = "HELLO!";
+ bool check = true;
 
  Console.WriteLine(a);
  Console.WriteLine(b);
@@ -12,32 +12,32 @@ bool check = true;
 
 //questão 2
 
-Console.WriteLine ("Digite dois numeros inteiros");
+ Console.WriteLine ("Digite dois numeros inteiros");
 
-int A = int.Parse(Console.ReadLine());
-int B = int.Parse(Console.ReadLine());
-int S = A + B;
-int D = A / B;
-int M = A * B;
-int Sub = A-B;
+ int A = int.Parse(Console.ReadLine());
+ int B = int.Parse(Console.ReadLine());
+ int S = A + B;
+ int D = A / B;
+ int M = A * B;
+ int Sub = A-B;
 
-Console.WriteLine("Soma = " + S);
-Console.WriteLine("Subtração = " + Sub);
-Console.WriteLine("Divisão = " + D);
-Console.WriteLine("Multiplicação = " + M);
+ Console.WriteLine("Soma = " + S);
+ Console.WriteLine("Subtração = " + Sub);
+ Console.WriteLine("Divisão = " + D);
+ Console.WriteLine("Multiplicação = " + M);
 
 //questão 3
 
-Console.WriteLine ("Digite seu nome e sua idade:");
+ Console.WriteLine ("Digite seu nome e sua idade:");
 
-string n = (Console.ReadLine());
-int i = int.Parse(Console.ReadLine());
+ string n = (Console.ReadLine());
+ int i = int.Parse(Console.ReadLine());
 
-Console.WriteLine ($"Olá, {n}, você tem {i} anos.");
+ Console.WriteLine ($"Olá, {n}, você tem {i} anos.");
 
 //questão 4
 
-Console.WriteLine("Digite um número: ");
+ Console.WriteLine("Digite um número: ");
         double x = double.Parse(Console.ReadLine());
 
         if (x > 0)
@@ -52,10 +52,10 @@ Console.WriteLine("Digite um número: ");
         {
             Console.WriteLine("O número é zero.");
         }
-
+        
 // questão 5
 
-for (int I = 1; I <= 10; I++)
+ for (int I = 1; I <= 10; I++)
     {
         Console.WriteLine(I);
     }
@@ -71,9 +71,28 @@ for (int I = 1; I <= 10; I++)
     }
         else if (idade >= 16 && idade < 18)
     {
-         Console.WriteLine("Você é menor de idade, mas já pode votar, poeém não esta apto(a) a ditigir");
+         Console.WriteLine("Você é menor de idade, mas já pode votar, porém não esta apto(a) a dirigir");
     }
         else
     {
         Console.WriteLine("Você é menor de idade, você ainda não está apto(a) a votar ou a dirigir");
     }
+
+//questão 7
+
+ Console.Write("Digite a primeira nota: ");
+  float n1 = float.Parse(Console.ReadLine());
+
+  Console.Write("Digite a segunda nota: ");
+  float n2 = float.Parse(Console.ReadLine());
+
+  Console.Write("Digite a terceira nota: ");
+  float n3 = float.Parse(Console.ReadLine());
+  float media = CalcularMedia(n1, n2, n3);
+
+  static float CalcularMedia(float n1, float n2, float n3)
+    {
+        return (n1 + n2 + n3) / 3;
+    }
+    Console.WriteLine("A média das notas é: " + media);
+// questão 8
